b3e71b957212cba37560bd8dfc569d9bb721ffe7
sayil1 <sundaysayil4u@gmail.com>
2020-02-19 11:24:28 +0100

    configure port for heroku upload

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
+const port = 5000;
 // mongoose.connect('mongodb://localhost/ideal', {useNewUrlParser: true});
  mongoose.connect('mongodb+srv://admin:sayil2194@cluster0-ueg0l.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
-mongoose.connect("mongodb://localhost:27017/ideal");
+// mongoose.connect("mongodb://localhost:27017/ideal");
 var cors = require('cors')
 app.use(cors())
 // parse application/json
@@ -20,4 +21,4 @@ app.use('/proj', require('./routes/projRoutes'))
 app.use("/cont", require('./routes/contRoutes'))
 app.use("/eve", require('./routes/eventsRoutes'))
  
-app.listen(5000)
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
